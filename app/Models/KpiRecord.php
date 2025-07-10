30caa79131437ef7caa167700c3473736d3cd16b
fahri <ahmadfachri846@gmail.com>
2025-07-10 15:34:22 +0700

    fitur hitung kpi dan role permission

---
+++ app/Models/KpiRecord.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KpiRecord extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kpi_records';
+    protected $guarded = [];
+
+    public function User()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function kpiMetric()
+    {
+        return $this->belongsTo(KpiMetrics::class, 'kpimetrics_id');
+    }
+}
